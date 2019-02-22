7f9790335905e7485349b346634377f7dbb77fdb
Snehal Srinivas Gandham <sgandham@sunnet.sj>
2019-02-22 10:38:20 -0800

    Modified Code

--- AgileIntPPMTool-branch1/AgileIntPPMTool-branch1/PPMToolFullStack/src/main/java/com/main/project/controller/ProjectController.java
+++ AgileIntPPMTool-branch1/AgileIntPPMTool-branch1/PPMToolFullStack/src/main/java/com/main/project/controller/ProjectController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
-
     @Autowired
     private ProjectService projectService;
     
---
+++ AgileIntPPMTool-branch1/AgileIntPPMTool-branch1/PPMToolFullStack/src/main/java/com/main/project/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.main.project.exceptions;
+
+/**
+ *
+ * @author sgandham
+ */
+public class ProjectIdExceptionResponse {
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+    
+}
